c9e6c6f4cb16e4387ec8c5fd8aabd93d2f71b08d
Eric Turcotte <eric2.0turcotte@gmail.com>
2021-01-24 22:29:07 -0500

    1er journee

---
+++ scriptsPseudoCode.js
@@ -0,0 +1,89 @@
+/*
+------------------------
+ Create a Guessing Game
+------------------------
+
+		START programm
+		DISPLAY game name
+			INIT welcoming animation
+
+		SHOW game patern
+			DISPLAY exemple of game board
+
+---------------
+Greet the User
+---------------
+
+		OBTAIN players NAME
+			INPUT player name
+
+		PRINT rules of GAME
+		
+		OBTAIN permission to lunch
+			GET player selection
+
+------------------
+Ask for difficulty
+------------------
+
+		PRINT difficulty LEVEL
+
+		GET players choice
+
+			GET player selection
+
+		INIT game base on player choice
+			GET confirmation of player choice
+
+---------------
+Start the Game
+---------------
+
+		DISPLAY "Players ready"
+
+		GET confirmation to START
+
+		INIT game base on player choice
+
+
+--------------
+GAME CONSTRUCT
+--------------
+
+
+		CREATE game
+
+		DISPLAY game board
+
+		CREATE gameplay
+
+				Initialize "number" to zero
+				Initialize counter to zero
+				GENERATE random number "random"
+				Hide random number
+				INPUT player choice "number"
+					IF "number" < "random" 
+						PRINT choose higher
+					IF "number" > "random" 
+						PRINT choose lower
+
+
+
+
+
+
+
+
+--------
+Results
+--------
+
+		SHOW result to player
+		INIT result base on succes
+		Get starting new game OR end game
+
+------------
+End Programm
+------------
+
+		*/
